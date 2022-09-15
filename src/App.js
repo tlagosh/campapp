d917bdc5c9a37d641ac7b82a7b75959f2c0a3dac
Tomás Lagos Hurtado <usr0060@iconstruye.com>
2022-09-15 01:03:42 -0300

    random and button chile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, { useState } from 'react';
 import './App.css';
 import Question from './Question';
+import Chile from './assets/chile.png';
+
+var sectionStyle = {
+  backgroundImage: "url(" + Chile + ")",
+  backgroundSize: 'cover',
+};
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
 
 function App() {
 
@@ -19,9 +33,7 @@ function App() {
     '¿Fecha cumpleaños Isa Vial?',
     '¿Fecha cumpleaños Rosario V?',
     '¿Fecha cumplaeños Trini C?',
-    '¿Cuáles son los pololos más pelaos?',
-    '¿cuantas minas se ha comido el gormi este año?',
-    '¿quien va ser el último miembro de campe en egresar?',
+    '¿Cuantas minas se ha comido el gormi este año?',
     '¿En qué trabaja el coloro?',
     '¿En qué trabaja el carepa?',
     '¿En qué trabaja el nelly?',
@@ -32,14 +44,44 @@ function App() {
     '¿Qué marca de chelas había en el primer campo?',
     '¿Como se llamaba la cabra que invitó a gori a su casa en pichilemu?',
     'Nombre de la calle donde estaba la cabaña de pichilemu',
+    'Quién mintió toda su infancia su fecha de nacimiento',
+    'Nombre 6 hermanos de Jose Pedro Claude',
+    'Nombre los 3 más altos de Campo',
+    'Cuántas regiones tiene Chile',
+    'Nombre dos partes de la cueca',
+    'Nombre a 8 de los jugadores de la selección chilena',
+    'Cuales son las ligas en las que ha jugado Campo FC',
+    'Cerveza favorita del Nelly',
+    'Conteo oficial de mujeres que han sido cortejadas con éxito por los solteros de Campo',
+    '¿Quiénes de campo han sido grabados o fotografiados vestidos de mujer?',
+    '¿Qué día festivo se pusieron a pololear Tomás con la Fran?',
+    'Nombre 5 paseos emblemáticos de Campo',
+    '¿A cuántos Campos asistió Clemente "el amateur" Correa?',
+    '¿En qué celebración se sacó a Clemente "el amateur" Correa por primera vez del grupo?',
+    '¿Cómo se llama la ex de Vicente Vial?',
+
   ];
+
+  shuffleArray(questions);
+
+  const handleNextQuestion = () => {
+    if (currentQuestion < questions.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+    }
+    else {
+      setCurrentQuestion(0);
+      alert('Se acabaron las preguntas');
+    }
+  };
 
   return (
     <div style={{ alignItems: 'center', justifyContent: 'center', flex: 1, textAlign: 'center', marginTop: '20%' }}>
       <style>{'body { background-color: #22cde0; }'}</style>
       <Question question={questions[currentQuestion]} />
       <div style={{ marginTop: 20 }}>
-        <button onClick={() => setCurrentQuestion(Math.floor(Math.random() * questions.length))} style={{ backgroundColor: '#c46637', color: 'white', fontSize: 20, padding: 15, borderRadius: 25, border: 'none' }}>Siguiente</button>
+        <button onClick={handleNextQuestion} style={{...sectionStyle, fontSize: 20, padding: 15, borderRadius: 25, border: 'none', color: 'transparent' }}>Siguiente
+          <p></p>
+        </button>
       </div>
     </div>
   );
